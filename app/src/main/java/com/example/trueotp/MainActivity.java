a6740ba0089b37e2fc532916315e187e2a0654ca
l3limp <kakarvardaan@gmail.com>
2021-07-09 16:49:56 +0530

    added signedin page n ui

--- app/src/main/java/com/example/trueotp/MainActivity.java
+++ app/src/main/java/com/example/trueotp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.trueotp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button generateOTPBtn, signInBtn;
+    EditText phoneNumber, OTP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
     }
+
+    private void findViews() {
+        generateOTPBtn = findViewById(R.id.generateOTP);
+        OTP = findViewById(R.id.OTP);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        signInBtn = findViewById(R.id.verify);
+    }
+
 }
