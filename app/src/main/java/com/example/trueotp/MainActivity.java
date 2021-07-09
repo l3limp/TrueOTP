07205295bf6d3071140be514b3b3096071113495
l3limp <kakarvardaan@gmail.com>
2021-07-09 18:34:25 +0530

    sign in working, but many errors

--- app/src/main/java/com/example/trueotp/MainActivity.java
+++ app/src/main/java/com/example/trueotp/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.trueotp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     Button generateOTPBtn, signInBtn;
-    EditText phoneNumber, OTP;
+    EditText phoneNumberET, OTPET;
+    String otp, phoneNumber;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback;
+    FirebaseAuth auth;
+    private String verificationCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +32,71 @@ public class MainActivity extends AppCompatActivity {
 
         findViews();
 
+        generateOTPBtn.setOnClickListener(v -> {
+
+            phoneNumber = phoneNumberET.getText().toString();
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                    phoneNumber,                        // Phone number to be verified
+                    60,                          // Timeout Duration
+                    TimeUnit.SECONDS,                   // Timeout unit
+                    MainActivity.this,           // Activity (for binding)
+                    mCallback                           // callback on verification state changed
+            );
+        });
+
+        StartFirebaseLogin();
+
+        signInBtn.setOnClickListener(v -> {
+            otp = OTPET.getText().toString();
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, otp);
+            SigninWithPhone(credential);
+        });
+
     }
 
     private void findViews() {
         generateOTPBtn = findViewById(R.id.generateOTP);
-        OTP = findViewById(R.id.OTP);
-        phoneNumber = findViewById(R.id.phoneNumber);
+        OTPET = findViewById(R.id.OTP);
+        phoneNumberET = findViewById(R.id.phoneNumber);
         signInBtn = findViewById(R.id.verify);
+    }
+
+    private void StartFirebaseLogin() {
+        auth = FirebaseAuth.getInstance();
+        mCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                Toast.makeText(MainActivity.this, "Verified!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(MainActivity.this, "Verification Failed!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                verificationCode = s;
+                Toast.makeText(MainActivity.this, "Code Sent", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private void SigninWithPhone(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+
+                    if(task.isSuccessful()) {
+                        startActivity(new Intent(MainActivity.this, SignedIn.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Incorrect OTP", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/example/trueotp/SignedIn.java
+++ app/src/main/java/com/example/trueotp/SignedIn.java
@@ -2,13 +2,49 @@ package com.example.trueotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignedIn extends AppCompatActivity {
+
+    TextView phoneNumber;
+    Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
+
+        setPhoneNumber();
+        findViews();
+
+        signOut.setOnClickListener(v-> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(SignedIn.this, MainActivity.class));
+            finish();
+        });
+
+    }
+
+    private void findViews() {
+        phoneNumber = findViewById(R.id.phoneNumberSignedIn);
+        signOut = findViewById(R.id.signOutButton);
+    }
+    
+    private void setPhoneNumber() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        try{
+            assert user != null;
+            phoneNumber.setText(user.getPhoneNumber());
+        }
+        catch (Exception e) {
+            Toast.makeText(SignedIn.this, "Could Not Retrieve Phone Number", Toast.LENGTH_SHORT).show();
+        }
     }
 }
