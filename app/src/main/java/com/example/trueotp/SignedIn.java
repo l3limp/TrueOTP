79cf0f895e26d6c86138d79bc4a5028ef1c0cbd6
l3limp <kakarvardaan@gmail.com>
2021-07-14 15:18:15 +0530

    complete

--- app/src/main/java/com/example/trueotp/SignedIn.java
+++ app/src/main/java/com/example/trueotp/SignedIn.java
@@ -2,28 +2,56 @@ package com.example.trueotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignedIn extends AppCompatActivity {
 
     Button signOut;
+    VideoView vid;
+    MediaController mediaController;
+    FloatingActionButton FAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
 
+        vid = findViewById(R.id.videoView);
         signOut = findViewById(R.id.signOutButton);
+        FAB = findViewById(R.id.play);
 
+        vid.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.confirm);
+        setMediaController();
+
+        FAB.setOnClickListener(V-> {
+            if(vid.isPlaying()) {
+                vid.pause();
+                FAB.setImageResource(R.drawable.ic_baseline_play_arrow_24);
+            }
+            else if(!vid.isPlaying()){
+                vid.start();
+                FAB.setImageResource(R.drawable.ic_baseline_pause_24);
+            }
+        });
         signOut.setOnClickListener(v-> {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(SignedIn.this, MainActivity.class));
             finish();
         });
-
+    }
+    void setMediaController() {
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(vid);
+        vid.setMediaController(mediaController);
+        vid.start();
     }
 }
